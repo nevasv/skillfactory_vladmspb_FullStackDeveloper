f45e573544628e7c88fcc0faf1e092c429e51ce8
vladmspb <nevasv@yandex.ru>
2022-09-21 20:25:45 +0300

    base
     Conflicts:
    	UserInput.py
    	base_work.py

--- Conditions/HW02.py
+++ Conditions/HW02.py
@@ -16,5 +16,29 @@ list_1 = [1, 2]
 
 list_2 = [1, 2, 3]
 val = list_2.pop()
-
+print("........")
 print(list_1 == list_2)
+print(id(list_1) == id(list_2))
+
+
+if int(input("Введи цену")) < 2700:
+    print("покупаем")
+else:
+    print("дорого")
+password = input("Введите пароль: ")
+if password:
+    pass1 = str(password)
+else:
+    print("Вы забыли ввести пароль")
+
+
+a, b = 1, 2
+
+min = a if a < b else b
+
+
+one = 12
+two = 30
+
+print(min if one % 2 and two % 2 else None)
+
